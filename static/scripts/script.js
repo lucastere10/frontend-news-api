20d4789adb187c6328c2ec5e4ad480dafaca25a2
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2023-09-24 06:45:38 -0300

    add: login and register js

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,23 +1,53 @@
-//FUNÇÃO PARA LOGAR ESSA MERDA
-function signup(e){
-    
-var user = {
-    email: email,
-    username: username,
-    password: pass,
-};
-
-var json = JSON.stringify(user);
-localStorage.setItem(username,json);
-console.log('user added');
-
-
-
-}
-
-
-function loginFunc(e){
+// Registration Function
+function register() {
     event.preventDefault();
-    console.log(123);
+    var username = document.getElementById('newUsername').value;
+    var password = document.getElementById('newPassword').value;
+    var email = document.getElementById('newEmail').value;
+
+    var user = {
+        username: username,
+        password: password,
+        email: email
+    }
+
+    var json = JSON.stringify(user);
+    localStorage.setItem(username, json);
+
+    alert('Registration Successful!');
+    console.log('Registration complete');
+    console.log(user);
+    console.log(window.location.href);
+    window.location.href = "login.html";
 }
+
+
+// Login Function
+function login() {
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    var user = localStorage.getItem(username);
+    console.log(user);
+    var data;
+
+    if (user) {
+        data = JSON.parse(user);
+    } else {
+        alert('User Not Found');
+        return;
+    }
+
+    console.log(data);
+
+    if(username == data.username && password == data.password){
+        alert('Login Successful');
+        window.location.href = "index.html";
+    } else {
+        alert('Wrong password');
+    }
+}
+
+
 
